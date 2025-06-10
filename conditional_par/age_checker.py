3efe19a8fb607d45fbad78eb2e9437d030a5361b
Muhammad Qasim <kingqasimmalik8@gmail.com>
2025-06-10 15:56:01 +0500

    Add 2 Paractice Projects into it

---
+++ conditional_par/age_checker.py
@@ -0,0 +1,10 @@
+age = 25
+
+if age < 13:
+    print("You're a Children")
+elif age < 20:
+    print("You're a Teenager")
+elif age < 60:
+    print("You're and Adult")
+else:
+    print("You're Senior")
---
+++ conditional_par/movie_ticket.py
@@ -0,0 +1,8 @@
+age = 22
+day = "Wednesday"
+price = 12 if age >= 18 else 8
+print("Simple Price", price)
+if day == "Wednesday":
+    price -= 2
+print("Wednesday Price", price)
+
